d259471413391063af96be40161f9b79d92a26f5
tigranavanesyan <tigran19910315@gmail.com>
2025-02-17 23:15:35 +0400

    root shadow for isolating styles

--- src/content-script.js
+++ src/content-script.js
@@ -1,11 +1,16 @@
 const preEls = document.querySelectorAll("pre");
 
 [...preEls].forEach((preEl) => {
+    const root = document.createElement("div");
+    const shadowRoot = root.attachShadow({ mode: "open" });
+
     const button = document.createElement("button");
     button.innerText = "Copy"
     button.type = "button"
 
-    preEl.prepend(button)
+    shadowRoot.prepend(button)
+
+    preEl.prepend(root);
 
     const codeEl = preEl.querySelector("code");
 
