c3414a3bbabbdc51138ee786deff337cb088bb92
tigranavanesyan <tigran19910315@gmail.com>
2025-02-17 23:05:03 +0400

    copy by created button

--- src/content-script.js
+++ src/content-script.js
@@ -1,1 +1,15 @@
-console.log('hello from extension')
+const preEls = document.querySelectorAll("pre");
+
+[...preEls].forEach((preEl) => {
+    const button = document.createElement("button");
+    button.innerText = "Copy"
+    button.type = "button"
+
+    preEl.prepend(button)
+
+    const codeEl = preEl.querySelector("code");
+
+    button.addEventListener("click", () => {
+        navigator.clipboard.writeText(codeEl.innerText);
+    });
+});
