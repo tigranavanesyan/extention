aed98c67d9ff2c086b2b8a2f94e0b0393416c501
tigranavanesyan <tigran19910315@gmail.com>
2025-02-18 14:19:51 +0400

    fetch from extention

--- src/background.js
+++ src/background.js
@@ -2,7 +2,8 @@ chrome.commands.onCommand.addListener((command) => {
     if (command === "copy-all") {
         getCurrentTabId().then((tabId) => {
             chrome.tabs.sendMessage(tabId, { action: "copy-all" },(allCode)=>{
-                console.log('allCode-----------',allCode)
+                // console.log('allCode-----------',allCode)
+                sendCodeToVScode(allCode)
             });
         });
     }
@@ -12,4 +13,13 @@ async function getCurrentTabId() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab.id;
+}
+
+function sendCodeToVScode(code) {
+    return fetch("http://localhost:4450/copypaste", {
+        method: "POST",
+        body: JSON.stringify({ code }),
+    }).catch((e) => {
+        console.log("vscode in not found");
+    });
 }
